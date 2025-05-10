7d42f44309e7afac184640a08cd525877255f0f2
Mihir Singh <mihirsingh604@gmail.com>
2025-05-09 19:35:13 -0700

    tables created in DB

--- db/populateDB.js
+++ db/populateDB.js
@@ -1,4 +1,4 @@
-const pool = require('./pool');
+const pool = require('./pool.js');
 
 const createUserTable = `
     CREATE TABLE IF NOT EXISTS users (
@@ -14,10 +14,22 @@ const createUserTable = `
     INSERT INTO users (first_name, last_name, username, password, membership_status, admin_status)
     VALUES ('mihir', 'singh', 'mihirsingh', 'Tennis0817*', 'yes', 'yes');
 `
+const createSessionsTable = `
+    CREATE TABLE IF NOT EXISTS "session" (
+        "sid" varchar NOT NULL COLLATE "default",
+        "sess" json NOT NULL,
+        "expire" timestamp(6) NOT NULL
+    )
+    WITH (OIDS=FALSE);
+    ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+    CREATE INDEX "IDX_session_expire" ON "session" ("expire");
+`
 
 async function createTables() {
     try {
         await pool.query(createUserTable);
+        await pool.query(createSessionsTable);
+        console.log("tables created in database");
     } catch (err) {
         console.log(err);
     }
