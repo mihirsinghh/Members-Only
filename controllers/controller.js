b7e46a2ab4fdc47a95e69503a96bb25514f4dec7
mihirsinghh <mihirsingh604@gmail.com>
2025-06-22 23:45:40 -0700

    added login page

--- controllers/controller.js
+++ controllers/controller.js
@@ -8,8 +8,13 @@ function loadSignUpForm(req, res) {
     res.render("signUpForm.ejs");
 }
 
+function loadLoginPage(req, res) {
+    res.render("login-page.ejs");
+}
+
 module.exports = {
     loadHomePage,
     loadSignUpForm,
+    loadLoginPage,
 };
 
--- routes/router.js
+++ routes/router.js
@@ -2,7 +2,8 @@ const {Router} = require('express'); //extracts Router constructor from express 
 const router = Router(); //creates new Router object so that routes can be exported
 const controller = require('../controllers/controller.js'); 
 
-router.get("/", controller.loadHomePage); //when the client sends a GET request to fetch some data from the server, the request is routed to the controller, which handles loading the home page
-router.get("/sign-up", controller.loadSignUpForm); //load the signup form when requested
+router.get("/", controller.loadHomePage);
+router.get("/sign-up", controller.loadSignUpForm);
+router.get("/log-in", controller.loadLoginPage);
 
 module.exports = router;
